45725e311f781a7ce6535085470f2ef2802578fc
bobo137 <pierre.calves13@gmail.com>
2022-04-11 15:39:34 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'cracra' : {
+        'ingredients' : {'onion': 5, 'basil': 5, 'pepper': 5, 'oregano': 5},
+        'price': 25,
     }
 }
